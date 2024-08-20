9ee1702fbeedd879adbc8ae328b2118039600f3a
swift-001 <121498784+swift-001@users.noreply.github.com>
2024-08-20 21:57:36 +0530

    Update page.tsx

--- app/problems/[ProblemID]/page.tsx
+++ app/problems/[ProblemID]/page.tsx
@@ -3,13 +3,13 @@ import dynamic from "next/dynamic"
 import { useParams } from "next/navigation"
 import { useEffect } from "react"
 
-const ExcalidrawWrapper = dynamic(
+/*const ExcalidrawWrapper = dynamic(
   async () =>
     (await import("../../../components/custom/excalidraw-wrapper")).default,
   {
     ssr: false,
   }
-)
+)*/
 
 export default function Page() {
   const params = useParams<{ ProblemID: string }>()
